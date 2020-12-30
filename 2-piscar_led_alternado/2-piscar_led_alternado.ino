b394e3e2166657ad64e213f09bb1bd9c6233a51a
GabrielVilelaPHS <GabrielVilelaPHS@users.noreply.github.com>
2020-12-29 22:41:23 -0300

    refatoração

--- 2-piscar_led_alternado/2-piscar_led_alternado.ino
+++ 2-piscar_led_alternado/2-piscar_led_alternado.ino
@@ -1,5 +1,4 @@
 void setup() {
-  // put your setup code here, to run once:
   pinMode(10, OUTPUT);
   pinMode(9, OUTPUT);
   pinMode(8, OUTPUT);
@@ -7,54 +6,37 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   int intervaloIda;
-  int intervaloVolta;
+  int intervaloPisca;
   
   intervaloIda = 500;
-  intervaloVolta = 200;
-  
-  digitalWrite(10, HIGH);
-  digitalWrite(9, LOW);
-  digitalWrite(8, LOW);
-  delay(intervaloIda);
-
-  digitalWrite(9, HIGH);
-  digitalWrite(10, LOW);
-  digitalWrite(8, LOW);
-  delay(intervaloIda);
-
-  digitalWrite(8, HIGH);
-  digitalWrite(10, LOW);
-  digitalWrite(9, LOW);
-  delay(intervaloVolta);
-
-  digitalWrite(8, HIGH);
-  digitalWrite(10, LOW);
-  digitalWrite(9, LOW);
-  
-  delay(intervaloIda);
-  digitalWrite(8, LOW);
-  delay(intervaloVolta);
-
-  digitalWrite(8, HIGH);
-  digitalWrite(10, LOW);
-  digitalWrite(9, LOW);
-  delay(intervaloIda);
-
-  digitalWrite(9, HIGH);
-  digitalWrite(10, LOW);
-  digitalWrite(8, LOW);
-  delay(intervaloIda);
+  intervaloPisca = 200;
 
   digitalWrite(10, HIGH);
-  digitalWrite(9, LOW);
-  digitalWrite(8, LOW);
-  delay(intervaloVolta);
-
   delay(intervaloIda);
-  digitalWrite(10, LOW);
-  delay(intervaloVolta);
 
+  digitalWrite(10, LOW);
+  digitalWrite(9, HIGH);
+  delay(intervaloIda);
+
+  digitalWrite(9, LOW);
+  digitalWrite(8, HIGH);
+  delay(intervaloIda);
+
+  digitalWrite(8, LOW);
+  delay(intervaloPisca);
+  digitalWrite(8, HIGH);
+  delay(intervaloIda);
+  
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
+  delay(intervaloIda);
+
+  digitalWrite(9, LOW);
+  digitalWrite(10, HIGH);
+  delay(intervaloIda);
+
+  digitalWrite(10, LOW);
+  delay(intervaloPisca);
  
 }
